e6ec25434e49a3244696c73da6f6adbabf5514af
danpoj <jws970306@khu.ac.kr>
2025-01-14 02:25:12 +0900

    update getTitleFromURL function

--- actions.ts
+++ actions.ts
@@ -26,6 +26,12 @@ export async function getTitleFromURL({ url }: { url: string }) {
       if (!match || typeof match[1] !== 'string') return 'No title found'; // if no title found, return "No title found"
       return match[1];
     })
+    .then((body: string) => {
+      const match = body.match(/<title([^<]*)>([^<]*)<\/title>/); // regular expression to parse contents of the <title> tag
+      if (!match || typeof match[match.length - 1] !== 'string')
+        return 'No title found'; // if no title found, return "No title found"
+      return match[match.length - 1];
+    })
     .catch((err) => {
       console.log(err);
       return 'No title found'; // if there's an error, return "No title found"

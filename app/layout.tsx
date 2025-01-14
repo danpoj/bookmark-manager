c85f7c79f53cd6b76a293455f35b38bfa4f8a624
danpoj <jws970306@khu.ac.kr>
2025-01-14 21:50:44 +0900

    base seos

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,25 +7,68 @@ import {
   HydrationBoundary,
   QueryClient,
 } from '@tanstack/react-query';
-import type { Metadata } from 'next';
-import { Geist, Geist_Mono } from 'next/font/google';
+import type { Metadata, Viewport } from 'next';
 import Image from 'next/image';
 import './globals.css';
-
-const geistSans = Geist({
-  variable: '--font-geist-sans',
-  subsets: ['latin'],
-});
-
-const geistMono = Geist_Mono({
-  variable: '--font-geist-mono',
-  subsets: ['latin'],
-});
+import { siteConfig } from '@/site-config';
 
 export const metadata: Metadata = {
-  title: 'Bookmark Manager',
-  description:
-    'supercharge your internet research and effortlessly organize your bookmarks.',
+  applicationName: '로나오프',
+  appleWebApp: {
+    capable: true,
+    statusBarStyle: 'default',
+    title: '로나오프',
+  },
+  formatDetection: {
+    telephone: false,
+  },
+
+  title: {
+    default: siteConfig.name,
+    template: `%s - ${siteConfig.name}`,
+  },
+  metadataBase: new URL(siteConfig.url),
+  description: siteConfig.description,
+  keywords: ['bookmark manager', 'bookmarks'],
+  authors: [
+    {
+      name: 'danpoj',
+      url: 'https://github.com/danpoj',
+    },
+  ],
+  creator: 'danpoj',
+  openGraph: {
+    type: 'website',
+    locale: 'ko_KR',
+    url: siteConfig.url,
+    title: siteConfig.name,
+    description: siteConfig.description,
+    siteName: siteConfig.name,
+    images: [
+      {
+        url: siteConfig.ogImage,
+        width: 1200,
+        height: 630,
+        alt: siteConfig.name,
+      },
+    ],
+  },
+  twitter: {
+    card: 'summary_large_image',
+    title: siteConfig.name,
+    description: siteConfig.description,
+    images: [siteConfig.ogImage],
+    creator: '@danpoj',
+  },
+  icons: {
+    icon: '/favicon.ico',
+    shortcut: '/favicon-16x16.png',
+    apple: '/apple-touch-icon.png',
+  },
+};
+
+export const viewport: Viewport = {
+  themeColor: '#FFFFFF',
 };
 
 export default async function RootLayout({
@@ -49,7 +92,7 @@ export default async function RootLayout({
   return (
     <html lang='en' suppressHydrationWarning>
       <body
-        className={`${geistSans.variable} ${geistMono.variable} bg-[#fafafa] dark:bg-[#0a0a0a] overscroll-none overflow-y-hidden`}
+        className={`bg-[#fafafa] dark:bg-[#0a0a0a] overscroll-none overflow-y-hidden`}
       >
         <ThemeProvider
           attribute='class'
---
+++ site-config.ts
@@ -0,0 +1,12 @@
+export const siteConfig = {
+  name: 'Bookmark Manager',
+  url: 'https://bm-danpoj.vercel.app/',
+  ogImage: 'https://bm-danpoj.vercel.app/opengraph-image.png',
+  description:
+    'supercharge your internet research and effortlessly organize your bookmarks.',
+  links: {
+    github: 'https://github.com/danpoj',
+  },
+};
+
+export type SiteConfig = typeof siteConfig;

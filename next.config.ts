d65e1c0148e3ee521fc6ca97523f5f9656b900d3
danpoj <jws970306@khu.ac.kr>
2025-01-15 17:28:15 +0900

    `minimumCacheTTL: 1 year`

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   reactStrictMode: false,
+  images: {
+    minimumCacheTTL: 31536000,
+  },
 };
 
 export default nextConfig;
